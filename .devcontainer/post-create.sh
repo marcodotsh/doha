6af3be8b143ac9eede9b05f94058867f069ffaf7
Marco Zanzottera <marcozanzottera@e.email>
2025-04-03 11:05:42 +0000

    Update devcontainer

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -13,7 +13,7 @@ DEBIAN_FRONTEND=noninteractive apt install -y \
     universal-ctags \
     verilator \
     wget
-pip3 install --break-system-packages \
+pip3 install \
     cocotb \
     cocotb-test \
     flake8 \
@@ -23,7 +23,8 @@ pip3 install --break-system-packages \
  
 # Verible
 ARCH=$(uname -m)
-if [[ $ARCH == "aarch64" ]]; then
+if [[ $ARCH == "aarch64" ]]
+then
     ARCH="arm64"
 fi
 DIST_ID=$(grep DISTRIB_ID /etc/lsb-release | cut -d'=' -f2)
@@ -31,31 +32,30 @@ DIST_RELEASE=$(grep RELEASE /etc/lsb-release | cut -d'=' -f2)
 DIST_CODENAME=$(grep CODENAME /etc/lsb-release | cut -d'=' -f2)
 VERIBLE_RELEASE=$(curl -s -X GET https://api.github.com/repos/chipsalliance/verible/releases/latest | jq -r '.tag_name')
 VERIBLE_TAR=verible-$VERIBLE_RELEASE-linux-static-$ARCH.tar.gz
-if [[ ! -f $VERIBLE_TAR ]]; then
+if [[ ! -f $VERIBLE_TAR ]]
+then
     wget https://github.com/chipsalliance/verible/releases/download/$VERIBLE_RELEASE/$VERIBLE_TAR
 fi
-if [[ ! -f "/usr/local/bin/verible-verilog-format" ]]; then
+if [[ ! -f "/usr/local/bin/verible-verilog-format" ]]
+then
     tar -C /usr/local --strip-components 1 -xf $VERIBLE_TAR
 fi
 rm $VERIBLE_TAR
 
 # Inspired from the Dockerfile at https://github.com/starwaredesign/vivado-docker
 
-#MIRROR_IP=
-#VIVADO_TAR_FILE=Xilinx_Vivado_SDK_2018.3_1207_2324
-#VIVADO_VERSION=2018.3
+VIVADO_TAR_FILE=Xilinx_Vivado_SDK_2018.3_1207_2324
+VIVADO_VERSION=2018.3
 
-if [[ ! -z $MIRROR_IP || -f ${VIVADO_TAR_FILE}.tar.gz ]]
+if [[ -f ${VIVADO_TAR_FILE}.tar.gz ]]
 then
 
 dpkg --add-architecture i386
 
+apt-get update
+
 apt-get install -y wget build-essential libglib2.0-0 libsm6 libxi6 libxrender1 libxrandr2 libfreetype6 libfontconfig1 locales git gawk iproute2 python3 gcc make net-tools libncurses5 libncurses5-dev tftpd zlib1g-dev libssl-dev flex bison libselinux1 gnupg git-core diffstat chrpath socat xterm autoconf libtool rsync texinfo gcc-multilib zlib1g:i386 lsb-release libtinfo5 dnsutils bc unzip
 
-if [ ! -f ${VIVADO_TAR_FILE}.tar.gz ]
-then
-wget ${MIRROR_IP}/${VIVADO_TAR_FILE}.tar.gz -q
-fi
 tar xzf ${VIVADO_TAR_FILE}.tar.gz
 ./${VIVADO_TAR_FILE}/xsetup --agree XilinxEULA,3rdPartyEULA,WebTalkTerms --batch Install --config ./.devcontainer/install_config.txt
 rm -rf ${VIVADO_TAR_FILE}/*
@@ -78,7 +78,33 @@ then
 
 dpkg --add-architecture i386
 
-apt-get install -y openfpgaloader minicom gcc-riscv64-unknown-elf libgl1 libnss3 libasound2t64 libqt5gui5t64
+apt-get install -y minicom gcc-riscv64-unknown-elf libgl1 libnss3 libasound2 libqt5gui5
+
+# Install openFPGALoader from source
+
+apt install -y \
+  git \
+  gzip \
+  libftdi1-2 \
+  libftdi1-dev \
+  libhidapi-hidraw0 \
+  libhidapi-dev \
+  libudev-dev \
+  zlib1g-dev \
+  cmake \
+  pkg-config \
+  make \
+  g++
+
+git clone https://github.com/trabucayre/openFPGALoader
+cd openFPGALoader
+mkdir build
+cd build
+cmake -DENABLE_UDEV=OFF ..
+cmake --build .
+make install
+cd ../..
+rm -rf ./openFPGALoader
 
 mkdir ./Gowin
 tar xzf ${GOWIN_TAR_FILE}.tar.gz -C ./Gowin
---
+++ README.md
@@ -0,0 +1,26 @@
+# DoHA containerized environment
+
+This repository contains the containerized environment with all the tools needed for Design of Hardware Accelerators lab sessions.
+
+It has been enriched with tools to support HDL development in Visual Studio Code and to automate the installation process of optional EDAs (Vivado and Gowin)
+
+Features:
+- VSCode Verilog-HDL/SystemVerilog/Bluespec SystemVerilog Extension
+- Icarus Verilog
+- Verilator
+- Verible
+- GTKWave
+- cocotb
+- (optional) automatic Vivado installation (v2018.3 tested)
+- (optional) automatic Gowin EDA installation (v1.9.11.01 tested)
+
+# How to use
+
+It is possible to start the development environment directly in Visual Studio Code or in the terminal
+
+# Credits
+
+This environment has been inspired by other publicly available online works:
+
+- [A Productive VSCode Setup for SystemVerilog Development - Igor Freire](https://igorfreire.com.br/2023/06/18/vscode-setup-for-systemverilog-development/)
+- [starwaredesign/vivado-docker](https://github.com/starwaredesign/vivado-docker)
